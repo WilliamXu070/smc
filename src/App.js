cda3084f25077f88f6c8ef5de13d57316425f7cf
William Xu <wxu9750@gmail.com>
2024-09-11 21:58:34 -0400

    changing template

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import './App.css';  // Assuming your CSS is saved as App.css
+import logoImage from './Logo.png';
 
 function App() {
   return (
     <div>
       <nav>
         <div className="navbar">
-          <div className="logo"><a href="#">CodingLab</a></div>
+          <div className="logo">
+            <img src={logoImage} alt="Logo" className="logo-image" />
+            <a href="#">SMC Dundas</a>
+          </div>
+
           <ul className="menu">
             <li><a href="#Home">Home</a></li>
-            <li><a href="#About">About</a></li>
-            <li><a href="#Category">Category</a></li>
+            <li><a href="#About">About Us</a></li>
+            <li><a href="#Gallery">Gallery</a></li>
+            <li><a href="#Donate">Donate</a></li>
             <li><a href="#Contact">Contact</a></li>
-            <li><a href="#Feedback">Feedback</a></li>
           </ul>
         </div>
       </nav>
       <section id="Home">Home Section</section>
       <section id="About">About Section</section>
-      <section id="Category">Category Section</section>
+      <section id="Gallery">Gallery Section</section>
+      <section id="Donate">Donate Section</section>
       <section id="Contact">Contact Section</section>
-      <section id="Feedback">Feedback Section</section>
       <div className="button">
         <a href="#Home"><i className="fas fa-arrow-up"></i></a>
       </div>
